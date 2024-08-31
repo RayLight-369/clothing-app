37b7c932331463da2346e53b72bc4c10a82942bc
Ray <buttrafay980@gmail.com>
2024-08-31 15:11:45 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,12 @@ import Link from "next/link";
 export default function Home () {
 
   return (
-    <main className="font-[Alata] flex h-full min-h-screen w-screen flex-col items-center justify-between">
-      <div className="flex justify-between w-screen min-h-[70vh] md:min-h-[95vh] bg-[linear-gradient(hsl(var(--background)/50%),hsl(var(--background)/75%)),url(/Imgs/hero-bg.jpg)] bg-center bg-cover bg-no-repeat bg-fixed">
+    <main className="font-[Alata] flex h-full min-h-screen w-screen flex-col ">
+      <h1 className="hidden md:block fixed z-0 tracking-tight md:text-[10rem] -left-36 opacity-10 top-[calc(50%+25px)] -translate-y-1/2  rotate-90">Fashion</h1>
+
+      <div id="hero" className="flex justify-between w-screen min-h-[70vh] md:min-h-[95vh] bg-[linear-gradient(hsl(var(--background)/50%),hsl(var(--background)/75%)),url(/Imgs/hero-bg.jpg)] bg-center bg-cover bg-no-repeat bg-fixed">
 
         <div className="hero-content flex flex-col w-full gap-9 justify-center items-center p-4 md:p-10">
-
           <h1 className="text-[2.75rem] md:text-8xl text-center leading-[1.1] w-full font-[Montserrat] font-black tracking-[0.0275rem] ">Explore Your <span className="md:text-background text-foreground [text-shadow:0px_0px_18px_hsl(var(--foreground)/20%)] md:[text-shadow:0px_1px_0_hsl(var(--foreground)),0px_0px_20px_hsl(var(--foreground)/40%)]">Style</span></h1>
           <p className="text-[1.15rem] text-foreground md:text-opacity-100 md:text-foreground md:text-2xl text-center w-[97%] md:w-[77%] text-pretty">Elevate your wardrobe with clothes that shine, simple styles that make you feel fine. Dress to impress, every single time.</p>
           <Link href={ "/store" } className='no-underline text-left w-[200px] px-10 py-2 bg-foreground text-background hover:shadow-md overflow-hidden transition-all duration-[250ms] hover:px-5 hover:-skew-y-3 hover:-skew-x-3 rounded-full relative after:transition-all after:duration-200 after:content-["→"] after:absolute after:text-[24px] after:-right-10 after:text-background after:block after:top-1/2 after:-translate-y-1/2 hover:after:right-5'>Start Exploring</Link>
@@ -18,7 +19,7 @@ export default function Home () {
 
       </div>
 
-      <div id="new-arrivals" className="w-screen h-full p-10">
+      <div id="new-arrivals" className="w-screen relative z-[5] bg-background h-full p-10">
 
         <h1 className="text-3xl font-bold text-center">New Arrivals</h1>
         <div id="content">
