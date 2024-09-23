33fa9c6a1700a1d506086dd312b258bf3f6de1a2
Ray <buttrafay980@gmail.com>
2024-09-23 19:36:19 +0500

    update

--- src/app/(app)/store/page.jsx
+++ src/app/(app)/store/page.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const page = () => {
   return (
-    <div>Clothes Store Coming Soon!</div>
+    <section className='w-screen h-screen'>
+      <div className='absolute text-5xl font-bold animate-pulse top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Clothes Store Coming Soon!
+
+      </div>
+    </section>
   );
 };
 
